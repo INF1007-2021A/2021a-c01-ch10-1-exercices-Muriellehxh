952079dd1b55bc6494dc808f7b5ad0d0304eebd2
Muriellehxh <dmui49@gmail.com>
2021-11-18 14:57:21 -0500

    ch10

--- exercice.py
+++ exercice.py
@@ -10,8 +10,9 @@ import matplotlib.pyplot as plt
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    m = int(math.sqrt(64))
-    return np.random.uniform(size=(m, m), low=-1.4, high=2.5)
+    m = int(64)
+    return np.linspace(-1.4, 2.5, 64) #UNIFORM ARRANGEMENT, dont use random.uniform
+
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
@@ -25,8 +26,9 @@ def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
+
     print(values)
-    index = np.abs(values-number).argmin()
+    index = np.abs(values-number).argmin() # on va a travers array de valeurs pour trouver difference la plus petite
 
     return values[index]
 
@@ -39,7 +41,7 @@ def graphique(x_values):
 # correction
 
     x = np.linspace(-1,1, num=250)
-    # NO! y = x**2 * math.sin(1/x**2) + x
+    #  y = x**2 * math.sin(1/x**2) + x  ==> NO!!!
 
     y = x**2 * np.sin(1/x**2) + x
 
@@ -49,21 +51,31 @@ def graphique(x_values):
 
 
 def pi():
-
-
 # methode montecarlo = cercle de rayon (chiffre a exterieur = exterieur de rayon
+   N = 10000
+
+   a = np.random.random(size = N)
+   b = np.random.random(size = N)
+   z = (a ** 2 + b ** 2) < 1
+   pi = 4*z.sum()/N
 
 
+
+   return pi
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    lin = (linear_values())
+    print(linear_values())
+
     cart = [[1,2], [2,5]]
+
     print(coordinate_conversion(np.array(cart)))
 
     print(find_closest_index(values=np.random.random(10), number=0.5))
 
 
     x_values = range(-1, 1)
-    graphique(x_values)
+    #graphique(x_values)
+
+    print(pi())
